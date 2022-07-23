3d949ae9a012105bf78e4da30ca7cd4647537bb0
Jeroen van Riel <jeroenvanriel@outlook.com>
2022-07-23 11:48:38 +0200

    rename server folder to backend

--- scripts/develop.sh
+++ scripts/develop.sh
@@ -5,7 +5,7 @@
 yarn watch &
 
 # Run the python server. It would be nice to restart this on changes, too!
-python sumo_web3d/sumo_web3d.py $@
+python backend/sumo_web3d.py $@
 
 # kill any remaining background processes
 jobs -p | xargs kill
